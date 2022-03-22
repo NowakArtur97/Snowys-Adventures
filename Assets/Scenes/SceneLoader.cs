d156e7ec19b041b5beeb25349ce4facc5b97050a
NowakArtur97 <nowart97@gmail.com>
2022-03-22 09:48:34 +0100

    Setup scene transitions with cutscenes

--- Assets/Scenes/SceneLoader.cs
+++ Assets/Scenes/SceneLoader.cs
@@ -2,10 +2,21 @@ using UnityEngine;
 
 public class SceneLoader : MonoBehaviour
 {
+    [SerializeField] public bool _shouldPlayStartAnimation = true;
+
+    private readonly string START_LEVEL_TRIGGER = "start";
     private readonly string END_LEVEL_TRIGGER = "end";
     private Animator _myAnimator;
 
-    private void Awake() => _myAnimator = GetComponent<Animator>();
+    private void Awake()
+    {
+        _myAnimator = GetComponent<Animator>();
+
+        if (_shouldPlayStartAnimation)
+        {
+            _myAnimator.SetTrigger(START_LEVEL_TRIGGER);
+        }
+    }
 
     public void StartEndLevelTransition() => _myAnimator.SetTrigger(END_LEVEL_TRIGGER);
 
