542a0977d5db32d7dff9003b79ca1d8ee18bbc64
NowakArtur97 <nowart97@gmail.com>
2022-03-24 09:45:08 +0100

    Create fifth level and animate fish after picking up

--- Assets/Cat/Scripts/WinTrigger.cs
+++ Assets/Cat/Scripts/WinTrigger.cs
@@ -2,17 +2,27 @@ using UnityEngine;
 
 public class WinTrigger : MonoBehaviour
 {
+    private readonly string DISSAPEAR_TRIGGER = "dissapear";
+
     private WinManager _winManager;
+    private Animator _animator;
     private bool _wasFound;
 
-    private void Awake() => _winManager = FindObjectOfType<WinManager>();
+    private void Awake()
+    {
+        _winManager = FindObjectOfType<WinManager>();
+        _animator = transform.parent.GetComponent<Animator>();
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (!_wasFound)
         {
+            AnimateTrigger();
             _winManager.FindFish();
             _wasFound = true;
         }
     }
+
+    private void AnimateTrigger() => _animator.SetTrigger(DISSAPEAR_TRIGGER);
 }
