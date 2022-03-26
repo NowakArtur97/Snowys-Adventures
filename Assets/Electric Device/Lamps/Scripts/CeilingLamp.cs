d84d1c3db973671542d38456acef47aac00595c9
NowakArtur97 <nowart97@gmail.com>
2022-03-26 08:17:00 +0100

    Add sounds for interactions with ceiling lamp

--- Assets/Electric Device/Lamps/Scripts/CeilingLamp.cs
+++ Assets/Electric Device/Lamps/Scripts/CeilingLamp.cs
@@ -4,6 +4,19 @@ public class CeilingLamp : Lamp
 {
     [SerializeField] private GameObject _lightSwitchOn;
     [SerializeField] private GameObject _lightSwitchOff;
+    [SerializeField] private AudioClip _turnOnSound;
+    [SerializeField] private AudioClip _turnOffSound;
+    [SerializeField] private float _soundMinPitch = 0.75f;
+    [SerializeField] private float _soundMaxPitch = 1.0f;
+
+    private AudioSource _myAudioSource;
+
+    protected override void Awake()
+    {
+        _myAudioSource = GetComponent<AudioSource>();
+
+        base.Awake();
+    }
 
     protected override void TurnOn()
     {
@@ -17,6 +30,15 @@ public class CeilingLamp : Lamp
         base.TurnOff();
 
         ChangeState(false, _lightSwitchOn, _lightSwitchOff);
+    }
+
+    public override void Interact()
+    {
+        base.Interact();
+
+        _myAudioSource.clip = CurrentState == ElectricDeviceState.ON ? _turnOnSound : _turnOffSound;
+        _myAudioSource.pitch = Random.Range(_soundMinPitch, _soundMaxPitch);
+        _myAudioSource.Play();
     }
 
     private void ChangeState(bool isOn, GameObject switchToEnable, GameObject switchToDisable)
