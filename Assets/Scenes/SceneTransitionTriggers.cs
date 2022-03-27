04902f71165e206bc6233d0efbc8461f24d8a49a
NowakArtur97 <nowart97@gmail.com>
2022-03-27 10:16:33 +0200

    Mute sounds untill the scene is loaded

--- Assets/Scenes/SceneTransitionTriggers.cs
+++ Assets/Scenes/SceneTransitionTriggers.cs
@@ -1,12 +1,29 @@
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class SceneTransitionTriggers : MonoBehaviour
 {
+    private readonly string SOUNDS_VOLUME_VARIABLE = "soundsVolume";
+    private readonly float MUTED_VOLUME = -80.0f;
+
+    [SerializeField] private AudioMixer _audioMixer;
+
     private Input _input;
+    private float _defaultSoundVolume;
 
-    private void Start() => _input = FindObjectOfType<Input>();
+    private void Start()
+    {
+        _input = FindObjectOfType<Input>();
+        _audioMixer.GetFloat(SOUNDS_VOLUME_VARIABLE, out _defaultSoundVolume);
+        _audioMixer.SetFloat(SOUNDS_VOLUME_VARIABLE, MUTED_VOLUME);
+    }
 
-    public void StartLevelTrigger() => _input.EnableMovemnt();
+    public void StartLevelTrigger()
+    {
+        _input.EnableMovemnt();
+
+        _audioMixer.SetFloat(SOUNDS_VOLUME_VARIABLE, _defaultSoundVolume);
+    }
 
     public void EndLevelTrigger() => LevelManager.LoadNextScene();
 }
