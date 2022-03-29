4a79d3bfa67ec5c0b9eabde77e4e5760ba017bd3
NowakArtur97 <nowart97@gmail.com>
2022-03-29 14:03:49 +0200

    Add fonts to UI

--- Assets/Cat/Scripts/WinManager.cs
+++ Assets/Cat/Scripts/WinManager.cs
@@ -3,20 +3,27 @@ using UnityEngine;
 
 public class WinManager : MonoBehaviour
 {
-    [SerializeField] TextMeshProUGUI _numberOfFishesText;
+    private string COUNTER_TEXT_GAME_OBJECT_NAME = "Fishes Counter Text (TMP)";
 
+    private TextMeshProUGUI _numberOfFishesText;
     private int _currentNumberOfFishes;
     private int _totalNumberOfFishes;
 
     private void Awake()
     {
         _totalNumberOfFishes = FindObjectsOfType<WinTrigger>().Length;
+        _numberOfFishesText = GameObject.Find(COUNTER_TEXT_GAME_OBJECT_NAME).GetComponent<TextMeshProUGUI>();
         _numberOfFishesText.text = _numberOfFishes;
     }
 
     public void FindFish()
     {
         _currentNumberOfFishes++;
+
+        if (_numberOfFishesText == null)
+        {
+            _numberOfFishesText = GameObject.Find(COUNTER_TEXT_GAME_OBJECT_NAME).GetComponent<TextMeshProUGUI>();
+        }
 
         _numberOfFishesText.text = _numberOfFishes;
 
